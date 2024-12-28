3477a3f301308bbdbf8476570ffe5b588644cd7e
Akhiltudekar <129061453+Akhiltudekar@users.noreply.github.com>
2024-12-28 14:04:13 +0530

    NZwalks.Api

---
+++ Controllers/RegionsController.cs
@@ -0,0 +1,196 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+
+        }
+        // GET ALL REGIONS
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            //Get Data From Database 
+            var regionsDomain = await dbContext.Regions.ToListAsync();
+
+            //Map Domain Models to DTOs 
+            var regionsDto = new List<RegionDto>();
+            foreach (var regionDomain in regionsDomain)
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = regionDomain.Id,
+                    Code = regionDomain.Code,
+                    Name = regionDomain.Name,
+                    RegionImageUrl = regionDomain.RegionImageUrl
+                });
+
+
+            }
+
+            return Ok(regionsDto);
+
+        }
+
+        //GET SINGLE REGION (Get region by id)
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+
+        {
+            // var region = dbContext.Regions.Find(id);
+            // Get Region Domain Model From Database 
+
+            var regionDomain = await dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if (regionDomain == null)
+            {
+                return NotFound();
+
+            }
+
+            //Map/Convert Region Domain Model To Region DTO 
+
+            var regionDto = new RegionDto
+            {
+
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl
+
+            };
+
+            // Return DTO back to client 
+
+            return Ok(regionDto);
+
+
+
+
+        }
+
+
+        //POST To Create New Region 
+        [HttpPost]
+
+        public async Task <IActionResult> Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            //Map or Convert DTO To Domain Model 
+            var regionDomainModel = new Region
+            {
+
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+
+
+            };
+
+            // Use Domain Model to create Region
+            await dbContext.Regions.AddAsync(regionDomainModel);
+            dbContext.SaveChangesAsync();
+
+            //Map Domain Model back to Dto 
+
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = regionDomainModel.Id }, regionDto);
+
+        }
+
+
+
+
+
+        //Update region 
+        //PUT 
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto)
+        {
+            //Check if region exists 
+            var regionDomainModel = await dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (regionDomainModel == null)
+            {
+                return NotFound();
+            }
+            //Map Dto to Domain model 
+            regionDomainModel.Code = updateRegionRequestDto.Code;
+            regionDomainModel.Name = updateRegionRequestDto.Name;
+            regionDomainModel.RegionImageUrl = updateRegionRequestDto.RegionImageUrl;
+
+            await dbContext.SaveChangesAsync();
+
+            // Conver Domain Model to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+
+            return Ok(regionDto);
+
+        }
+
+
+        //Delete Region 
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task <IActionResult> Delete([FromRoute] Guid id)
+        {
+            var regionDomainModel = await dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if (regionDomainModel == null)
+            {
+                return NotFound();
+
+
+            }
+            //Delete region
+            dbContext.Regions.Remove(regionDomainModel);
+            await dbContext.SaveChangesAsync();
+
+            // return deleted Region back 
+            //map Domain Model to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+
+            return Ok(regionDto);
+        }
+    }
+   
+   }
+
+
+
+
+
---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+
+            string[] studentNames = new string[] { "akhil", "jane", "mark", };
+
+            return Ok(studentNames);
+
+        }
+    }
+}
